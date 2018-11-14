2bbf9c1030f0280711a508c75e394d9188c53fa7
dariusmandres@yahoo.ca <dariusmandres@yahoo.ca>
2018-11-14 15:52:57 -0500

    made some changes to init.py

--- server/__init__.py
+++ server/__init__.py
@@ -1,16 +1,28 @@
 from flask import Flask
-from config import DevelopmentConfig
 from server.database import db
 from server.models import User
 from server import testData
+from pathlib import Path
+import os
 
+# creates the flask app
 def create_app():
 
     # creates the app
     app = Flask(__name__, static_folder='../static/dist', template_folder='../static/templates')
 
-    # configuration
-    app.config.from_object(DevelopmentConfig)
+    # config
+    devConfig = Path(os.path.join(app.instance_path, 'devConfig.py'))
+    prodConfig = Path(os.path.join(app.instance_path, 'prodConfig.py'))
+
+    # dev over prod
+    if devConfig.is_file():
+        from instance.devConfig import DevelopmentConfig
+        app.config.from_object(DevelopmentConfig)
+
+    elif prodConfig.is_file():
+        from instance.prodConfig import ProductionConfig
+        app.config.from_object(ProductionConfig)
 
     # database
     with app.app_context():
